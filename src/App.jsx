595ea3e4cfb590635f07c405b19a81f57500bc7f
Allan <allan.landin25@gmail.com>
2024-07-07 14:12:43 -0300

    add the  component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 import ProductsDisplay from "./components/ProductsDisplay";
 import Header from "./components/Header";
+import Intro from "./components/Intro";
 
 function App() {
   return (
     <div className="px-5">
       <Header />
+      <Intro />
       <ProductsDisplay />
     </div>
   );
