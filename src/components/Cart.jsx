3e41dad4a77ed4d768ea99ee3b1dc32e7ac803d2
Allan <allan.landin25@gmail.com>
2024-07-13 10:22:31 -0300

    improve cart ui

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import CartItem from "./CartItem";
 import { appContext } from "../contexts/appContext";
+import formatCurrency from "../utils/formatCurrency";
 
 function Cart() {
   const [products, setProducts, modalIsVisible, setModalIsVisible] =
@@ -15,19 +16,39 @@ function Cart() {
   ) : (
     <div
       id="cart"
-      className="z-50 bg-red-800 w-full sm:w-1/2 md:1/3 h-full fixed top-0 right-0 p-3 sm:rounded-s-lg text-white transition duration-150"
+      className="z-50 bg-red-500 w-full sm:w-1/2 md:1/3 h-full fixed top-0 right-0 p-3 sm:rounded-s-lg text-white transition duration-150 flex flex-col gap-2"
     >
-      <button onClick={closeModal} className="flex flex-col">
+      <button onClick={closeModal} className="flex flex-col w-full">
         <i className="fa-solid fa-xmark self-end text-3xl hover:cursor-pointer hover:text-red-500"></i>
       </button>
-      <div>
-        <h2 className="text-xl underline text-center mb-2">Carrinho</h2>
+      <div className="flex justify-between">
+        <h2 className="text-2xl underline mb-2">
+          Carrinho ({products.length})
+        </h2>
+        <button className="bg-white rounded text-black p-2 hover:bg-slate-100">
+          Apagar tudo
+        </button>
       </div>
-      <ul className="flex flex-col gap-5 scroll-auto">
+      <ul className="flex flex-col gap-5 overflow-auto flex-grow underline underline-offset-8">
         {products.map((product) => (
           <CartItem product={product} />
         ))}
       </ul>
+      <div className="flex flex-col gap-5 text-xl border-t py-2">
+        <div className="flex justify-between">
+          <p>Total de produtos ({products.length})</p>
+          <p>
+            {formatCurrency(
+              products.reduce((acc, curr) => acc + curr.price, 0)
+            )}
+          </p>
+        </div>
+        {products.length > 0 && (
+          <button className="bg-green-500 rounded hover:bg-green-300 p-2">
+            Finalizar compra
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,15 +1,18 @@
 import formatCurrency from "../utils/formatCurrency";
 function CartItem({ product }) {
   return (
-    <div className="flex gap-2 p-3 shadow-3lg rounded bg-slate-50 text-black">
+    <div className="overflow-auto flex justify-between gap-2 p-3 shadow-3lg rounded bg-slate-50 text-black h-1/4">
       <img
-        className="size-1/3 object-contain"
+        className="object-cover"
         src={product.thumbnail.replace(/\w\.jpg/gi, "W.jpg")}
       ></img>
-      <div className="flex flex-col justify-between text-xl">
-        <h2 className="">{product.title.slice(0, 60)}...</h2>
+      <div className="flex-grow flex flex-col justify-between text-xl">
+        <h2 className="">{product.title.slice(0, 50)}...</h2>
         <p className="">{formatCurrency(product.price)}</p>
       </div>
+      <button>
+        <i className="text-lg hover:text-red-500 fa-solid fa-trash"></i>
+      </button>
     </div>
   );
   q;
